e0e12c1b1163088b4a21a334277efa4aa9ca80e6
carlos <carlosramos10k@gmail.com>
2023-07-27 14:06:41 -0300

    Metodo findByDepartment implementado

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,25 @@ import model.dao.SellerDao;
 
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("TESTE 1: SELLER findById");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println("_--------------------------------_");
+
+        System.out.println("TSTE 2: SELLER findByDepartment");
+        List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
+
+        for(Seller obj : list){
+            System.out.println(obj);
+        }
 
     }
 }
