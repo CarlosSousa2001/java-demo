324f86a1f1d183234258781471858bde37a2a340
carlos <carlosramos10k@gmail.com>
2023-07-27 12:33:30 -0300

    Adição da classe Seller

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+
         Department department = new Department(10, "teste/QA");
 
+        Seller seller = new Seller(10, "Carlos", "carlos@gmail.com",new Date(), 2000.00, department);
+
         System.out.println(department.toString());
+
+        System.out.println("_-----------------------_");
+
+        System.out.println(seller.toString());
     }
 }
